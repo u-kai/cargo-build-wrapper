8993363f16a27d87ba94f7ee38b7dbdf2b882da9
u-kai <76635578+u-kai@users.noreply.github.com>
2023-04-25 20:24:09 +0900

    add fn builder

--- src/new/sample_code.rs
+++ src/new/sample_code.rs
@@ -1,39 +1,79 @@
+use std::collections::HashMap;
+
 #[derive(Debug)]
 pub struct MainBuilder {
-    content: String,
-    attr: Option<String>,
-    async_mode: bool,
+    inner: FnBuilder,
 }
 
 impl MainBuilder {
-    const SPACE: &'static str = "    ";
     pub fn new() -> Self {
         Self {
-            content: String::new(),
-            attr: None,
-            async_mode: false,
+            inner: FnBuilder::new("main"),
         }
     }
     pub fn attr(self, attr: impl Into<String>) -> Self {
         Self {
-            content: self.content,
-            attr: Some(attr.into()),
-            async_mode: self.async_mode,
+            inner: self.inner.attr(attr),
         }
     }
     pub fn async_mode(self) -> Self {
         Self {
-            content: self.content,
-            attr: self.attr,
+            inner: self.inner.async_mode(),
+        }
+    }
+    pub fn build(self) -> String {
+        self.inner.build()
+    }
+    pub fn add_line(self, line: &str) -> Self {
+        Self {
+            inner: self.inner.add_line(line),
+        }
+    }
+}
+
+type Arg = String;
+type Type = String;
+#[derive(Debug)]
+struct FnBuilder {
+    name: String,
+    content: String,
+    args: HashMap<Arg, Type>,
+    retr: Option<Type>,
+    attr: Option<String>,
+    async_mode: bool,
+}
+
+impl FnBuilder {
+    const SPACE: &'static str = "    ";
+    fn new(name: impl Into<String>) -> Self {
+        Self {
+            name: name.into(),
+            content: String::new(),
+            attr: None,
+            async_mode: false,
+            args: HashMap::new(),
+            retr: None,
+        }
+    }
+    pub fn attr(self, attr: impl Into<String>) -> Self {
+        Self {
+            attr: Some(attr.into()),
+            ..self
+        }
+    }
+    pub fn async_mode(self) -> Self {
+        Self {
             async_mode: true,
+            ..self
         }
     }
     pub fn build(self) -> String {
         format!(
-            "{}{} {{{}
+            "{}{}{} {{{}
 }}",
             self.create_attr(),
-            self.create_prefix_main(),
+            self.create_prefix_fn(),
+            self.create_return(),
             self.content
         )
     }
@@ -41,24 +81,50 @@ impl MainBuilder {
         self.content = format!("{}\n{}{}", self.content, Self::SPACE, line);
         self
     }
+    fn create_return(&self) -> String {
+        self.retr
+            .as_ref()
+            .map(|s| format!(" -> {}", s))
+            .unwrap_or_default()
+    }
     fn create_attr(&self) -> String {
         self.attr
             .as_ref()
             .map(|s| format!("#[{}]\n", s))
             .unwrap_or_default()
     }
-    fn create_prefix_main(&self) -> String {
+    fn create_prefix_fn(&self) -> String {
         if self.async_mode {
-            String::from("async fn main()")
+            format!("async fn {}()", self.name)
         } else {
-            String::from("fn main()")
+            format!("fn {}()", self.name)
         }
     }
 }
-
 #[cfg(test)]
 mod tests {
     use super::*;
+    #[test]
+    fn integration_test() {
+        let main_str = FnBuilder::new("main")
+            .attr("actix_web::main")
+            .async_mode()
+            .add_line("HttpServer::new(|| App::new().service(hello).service(echo))")
+            .add_line(r#"    .bind(("127.0.0.1", 8080))?"#)
+            .add_line(r#"    .run()"#)
+            .add_line(r#"    .await"#)
+            .build();
+        assert_eq!(
+            main_str,
+            r#"#[actix_web::main]
+async fn main() {
+    HttpServer::new(|| App::new().service(hello).service(echo))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
+}"#
+        );
+    }
     #[test]
     fn main関数に属性を付与することができる() {
         let main_str = MainBuilder::new().attr("tokio::main").build();
