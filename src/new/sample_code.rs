96be3f06a593c331cc54b8adb3aa3e82435bae4f
u-kai <76635578+u-kai@users.noreply.github.com>
2023-04-25 20:00:12 +0900

    add muilti line

--- src/new/sample_code.rs
+++ src/new/sample_code.rs
@@ -1,25 +1,24 @@
 #[derive(Debug)]
-struct MainBuilder {
+pub struct MainBuilder {
     content: String,
 }
 
 impl MainBuilder {
     const SPACE: &'static str = "    ";
-    fn new() -> Self {
+    pub fn new() -> Self {
         Self {
             content: String::new(),
         }
     }
-    fn build(self) -> String {
+    pub fn build(self) -> String {
         format!(
-            "fn main() {{
-{}{}}}",
-            Self::SPACE,
+            "fn main() {{{}
+}}",
             self.content
         )
     }
-    fn add_line(mut self, line: &str) -> Self {
-        self.content = format!("{}{}\n{}", self.content, line, Self::SPACE);
+    pub fn add_line(mut self, line: &str) -> Self {
+        self.content = format!("{}\n{}{}", self.content, Self::SPACE, line);
         self
     }
 }
@@ -28,22 +27,45 @@ impl MainBuilder {
 mod tests {
     use super::*;
     #[test]
+    fn async_main関数を出力することができる() {
+        //let main_str = MainBuilder::new().async_mode().build();
+
+        //assert_eq!(
+        //main_str,
+        //"async fn main() {
+        //}"
+        //);
+
+        //let main_str = MainBuilder::new()
+        //.add_line("let mut map = std::collections::HashMap::new();")
+        //.build();
+
+        //assert_eq!(
+        //main_str,
+        //"fn main() {\n    let mut map = std::collections::HashMap::new();\n    }"
+        //);
+    }
+    #[test]
     fn main関数を出力することができる() {
         let main_str = MainBuilder::new().build();
 
         assert_eq!(
             main_str,
             "fn main() {
-    }"
+}"
         );
 
         let main_str = MainBuilder::new()
             .add_line("let mut map = std::collections::HashMap::new();")
+            .add_line(r#"map.insert("key","value");"#)
             .build();
 
         assert_eq!(
             main_str,
-            "fn main() {\n    let mut map = std::collections::HashMap::new();\n    }"
+            r#"fn main() {
+    let mut map = std::collections::HashMap::new();
+    map.insert("key","value");
+}"#
         );
     }
 }
