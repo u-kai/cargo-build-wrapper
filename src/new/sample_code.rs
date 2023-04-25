6f7bed37f946bf1867a798a8e019ab115ea18251
u-kai <76635578+u-kai@users.noreply.github.com>
2023-04-25 20:03:33 +0900

    add async fn

--- src/new/sample_code.rs
+++ src/new/sample_code.rs
@@ -1,6 +1,7 @@
 #[derive(Debug)]
 pub struct MainBuilder {
     content: String,
+    async_mode: bool,
 }
 
 impl MainBuilder {
@@ -8,18 +9,33 @@ impl MainBuilder {
     pub fn new() -> Self {
         Self {
             content: String::new(),
+            async_mode: false,
+        }
+    }
+    pub fn async_mode(self) -> Self {
+        Self {
+            content: self.content,
+            async_mode: true,
         }
     }
     pub fn build(self) -> String {
         format!(
-            "fn main() {{{}
+            "{} {{{}
 }}",
+            self.create_prefix_main(),
             self.content
         )
     }
     pub fn add_line(mut self, line: &str) -> Self {
         self.content = format!("{}\n{}{}", self.content, Self::SPACE, line);
         self
+    }
+    fn create_prefix_main(&self) -> String {
+        if self.async_mode {
+            String::from("async fn main()")
+        } else {
+            String::from("fn main()")
+        }
     }
 }
 
@@ -28,13 +44,13 @@ mod tests {
     use super::*;
     #[test]
     fn async_main関数を出力することができる() {
-        //let main_str = MainBuilder::new().async_mode().build();
+        let main_str = MainBuilder::new().async_mode().build();
 
-        //assert_eq!(
-        //main_str,
-        //"async fn main() {
-        //}"
-        //);
+        assert_eq!(
+            main_str,
+            "async fn main() {
+}"
+        );
 
         //let main_str = MainBuilder::new()
         //.add_line("let mut map = std::collections::HashMap::new();")
