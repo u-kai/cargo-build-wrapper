83830dd6fa331eb05d953d7a72237167f972b9db
u-kai <76635578+u-kai@users.noreply.github.com>
2023-04-25 19:53:16 +0900

    add space

--- src/new/sample_code.rs
+++ src/new/sample_code.rs
@@ -1,8 +1,10 @@
+#[derive(Debug)]
 struct MainBuilder {
     content: String,
 }
 
 impl MainBuilder {
+    const SPACE: &'static str = "    ";
     fn new() -> Self {
         Self {
             content: String::new(),
@@ -11,12 +13,13 @@ impl MainBuilder {
     fn build(self) -> String {
         format!(
             "fn main() {{
-    {}}}",
+{}{}}}",
+            Self::SPACE,
             self.content
         )
     }
     fn add_line(mut self, line: &str) -> Self {
-        self.content = format!("{}{}\n    ", self.content, line);
+        self.content = format!("{}{}\n{}", self.content, line, Self::SPACE);
         self
     }
 }
